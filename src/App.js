5d88d66d1b6a66e5c139a0b97fd74e628e435064
--global <juanp.robles@hotmail.com>
2021-11-27 12:40:49 +0100

    Feat: Added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage.component';
+
+
+const HatsPage = () => {
+  return(
+    <h1>
+      HATS PAGE
+    </h1>
+  )
+} 
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,25 +13,25 @@ class Directory extends React.Component {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
-          linkUrl: 'shop/hats'
+          linkUrl: 'hats'
         },
         {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2,
-          linkUrl: 'shop/jackets'
+          linkUrl: ''
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3,
-          linkUrl: 'shop/sneakers'
+          linkUrl: ''
         },
         {
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
-          linkUrl: 'shop/womens',
+          linkUrl: '',
           id: 4
 
         },
@@ -50,8 +50,10 @@ class Directory extends React.Component {
     return (
       <div className="directory-menu">
         {
-          this.state.sections.map(({ title, imageUrl, id, size }) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+          // this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+          //   <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl}/>
+          this.state.sections.map(({ id, ...otherSectionProps }) => (  //This two lines are the same as the ones on top
+            <MenuItem key={id} {...otherSectionProps} />
           ))
         }
       </div>
