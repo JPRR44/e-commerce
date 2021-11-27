6bd75af408f7376b922d90dddd7bd813ca23c1e8
--global <juanp.robles@hotmail.com>
2021-11-27 14:46:52 +0100

    Feat: Added shopPage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import HomePage from './pages/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-
-const HatsPage = () => {
-  return(
-    <h1>
-      HATS PAGE
-    </h1>
-  )
-} 
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
