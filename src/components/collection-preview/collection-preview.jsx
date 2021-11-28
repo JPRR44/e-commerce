6b06568c6d3294a355c9bc088ce3d97d56118311
JPRR44 <juanp.robles@hotmail.com>
2021-11-28 21:57:21 +0100

    Feat: Added collection item to shop page

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './collection-preview.styles.scss';
-
+import '../collection-item/collection-item';
+import CollectionItem from '../collection-item/collection-item';
 
 const CollectionPreview = ({ title, items }) => {
   return (
@@ -10,8 +11,8 @@ const CollectionPreview = ({ title, items }) => {
         {
           items
             .filter((item, idx) => idx < 4)
-            .map((item) => (
-              <div key={item.id}>{item.name}</div>
+            .map(({id, ...otherCollectionProps}) => (
+              <CollectionItem key={id} {...otherCollectionProps}/>
             ))
         }
       </div>
